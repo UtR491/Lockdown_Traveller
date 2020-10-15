b306067c4be26d3d6897343da5d8bba9be0322fc
singhabhyudita <singhabhyudita@gmail.com>
2020-10-15 22:16:33 +0530

    Server Class

---
+++ LockdownTraveller/src/Server/Server.java
@@ -0,0 +1,39 @@
+package Server;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket;
+        DatabaseConnector db = null;
+
+        try {
+            serverSocket = new ServerSocket(1234);
+            db = new DatabaseConnector();
+            db.getConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true) {
+            try {
+                assert serverSocket != null;
+                System.out.println("Waiting for a customer");
+                socket = serverSocket.accept();
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                System.out.println("customer connected now creating database connection");
+                System.out.println("database connected now going to request identifier.");
+                Thread t = new Thread(new RequestIdentifier(socket, oos, ois, db));
+                t.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
