728bd84d53b8e68a03735779ea2ea6e81d9a9e02
singhabhyudita <60813273+singhabhyudita@users.noreply.github.com>
2020-10-18 20:09:22 +0530

    Update DatabaseConnector.java

--- LockdownTraveller/src/Server/DatabaseConnector.java
+++ LockdownTraveller/src/Server/DatabaseConnector.java
@@ -119,6 +119,120 @@ public class DatabaseConnector {
         }
         return null;
     }
+    private static int DayToDate(String sDate) {
+        String[] date = sDate.split("/");
+        System.out.println("Printing stuff"+date[0]);
+        System.out.println(date[1]);
+        System.out.println(date[2]);
+        LocalDate localDate = LocalDate.of(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        return dayOfWeek.getValue();
+    }
+    DisplayTrainsResponse DisplayTrains (String query1,String query2,String query3,String query4,String query5,String query6,String sDate,String source,String dest) throws IOException {
+        char[] Days_Running = null;
+        ResultSet result = null;
+        int required_day = DatabaseConnector.DayToDate(sDate);
+        PreparedStatement preparedStatement;
+        try {
+            assert null != connection;
+            preparedStatement = connection.prepareStatement(query1, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String [] Train_ID = new String[5],Train_Name = new String[5],Departure=new String[5],Arrival=new String[5],First_AC=new String[5],Second_AC=new String[5],Third_AC=new String[5],Sleeper=new String[5];
+        String Date = null,Source=null,Destination=null;
+        int i=0;
+        while (true) {
+            try {
+                assert result != null;
+                if (!result.next()) break;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            String Day_No = null;
+            try {
+                Day_No = result.getString("Day_No");
+                Days_Running = result.getString("Days_Running").toCharArray();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            assert Days_Running != null;
+            assert Day_No != null;
+            char value = Days_Running[required_day - Integer.parseInt(Day_No)];
+            if (value == '1') {
+                try {
+                    assert false;
+                    Train_ID[i]=result.getString("Train_ID");
+                    Train_Name[i]=result.getString("Train_Name");
+                    Source=source;
+                    Departure[i]=result.getString("Departure");
+                    Destination=dest;
+                    Arrival[i]=result.getString("Arrival");
+                    Date=sDate;
+
+
+                    String q = query2.replaceFirst("xxxxx", Train_ID[i]);
+                    preparedStatement=connection.prepareStatement(q);
+                    ResultSet resultSet=preparedStatement.executeQuery();
+
+                    String q2=query3.replaceFirst("xxxxx",Train_ID[i]);
+                    preparedStatement=connection.prepareStatement(q2);
+                    ResultSet SL_Seats=preparedStatement.executeQuery();
+                    resultSet.next();
+                    SL_Seats.next();
+                    int available_seats = 0;
+                    if(resultSet.getString("Sleeper_Seats")!=null) {
+                         available_seats=Integer.parseInt(resultSet.getString("Sleeper_Seats"))-Integer.parseInt(SL_Seats.getString(1));
+                    }
+                    Sleeper[i]=String.valueOf(available_seats);
+
+
+                    String q3=query4.replaceFirst("xxxxx",Train_ID[i]);
+
+                    preparedStatement=connection.prepareStatement(q3);
+                    ResultSet AC1_Seats=preparedStatement.executeQuery();
+                    AC1_Seats.next();
+                    if(resultSet.getString("FirstAC_Seats")!=null)available_seats=Integer.parseInt(resultSet.getString("FirstAC_Seats"))-Integer.parseInt(AC1_Seats.getString(1));
+                    First_AC[i]=String.valueOf(available_seats);
+
+                    String q4=query5.replaceFirst("xxxxx",Train_ID[i]);
+                    preparedStatement=connection.prepareStatement(q4);
+                    ResultSet AC2_Seats=preparedStatement.executeQuery();
+                    AC2_Seats.next();
+                    if(resultSet.getString("SecondAC_Seats")!=null)available_seats=Integer.parseInt(resultSet.getString("SecondAC_Seats"))-Integer.parseInt(AC2_Seats.getString(1));
+                    Second_AC[i]=String.valueOf(available_seats);
+
+
+                    String q5=query6.replaceFirst("xxxxx",Train_ID[i]);
+                    preparedStatement=connection.prepareStatement(q5);
+                    ResultSet AC3_Seats=preparedStatement.executeQuery();
+                    AC3_Seats.next();
+                    if(resultSet.getString("ThirdAC_Seats")!=null)available_seats=Integer.parseInt(resultSet.getString("ThirdAC_Seats"))-Integer.parseInt(AC3_Seats.getString(1));
+                    Third_AC[i]=String.valueOf(available_seats);
+
+                    i++;
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+         return new DisplayTrainsResponse(Train_ID,Train_Name,Source,Departure,Destination,Arrival,First_AC,Second_AC,Third_AC,Sleeper,Date,i);
+
+    }
+    CancelBookingReponse CancelBooking(String query,ObjectOutputStream oos) throws SQLException, IOException {
+        PreparedStatement preparedStatement=connection.prepareStatement(query);
+        int result=preparedStatement.executeUpdate();
+        String response = null;
+        if(result==0){response="No bookings found under the given PNR.Failed to cancel the booking";}
+        else if(result>0){response="Booking cancelled succesfully";}
+        return new CancelBookingResponse(response);
+        
+    }
 
     public void getConnection() {
         try {
