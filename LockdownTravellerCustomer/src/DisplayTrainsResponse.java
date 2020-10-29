5522f39d1bd0c38c4d2ffacf9561a64ecf66387d
Utkarsh Rai <utkarshrai491@gmail.com>
2020-10-29 16:35:00 +0530

    Remove conflict.

--- LockdownTravellerCustomer/src/DisplayTrainsResponse.java
+++ LockdownTravellerCustomer/src/DisplayTrainsResponse.java
@@ -5,24 +5,40 @@ public class DisplayTrainsResponse extends Response implements Serializable
 {
     final private ArrayList<String> Train_ID,Train_Name,Departure,Arrival,First_AC,Second_AC,Third_AC,Sleeper;
     final private String sDate,Source,Destination;
-    final private int i;
+    final private ArrayList<Integer>AC1Fare, AC2Fare,AC3Fare,SLFare;
 
-    public DisplayTrainsResponse(ArrayList<String> Train_ID, ArrayList<String> Train_Name, String Source,
-                                 ArrayList<String> Departure, String Destination, ArrayList<String> Arrival,
-                                 ArrayList<String> First_AC, ArrayList<String> Second_AC, ArrayList<String> Third_AC,
-                                 ArrayList<String> Sleeper, String sDate, int i) {
-        this.Train_ID=Train_ID;
-        this.Train_Name=Train_Name;
-        this.Source=Source;
-        this.Departure=Departure;
-        this.Destination=Destination;
-        this.Arrival=Arrival;
-        this.First_AC=First_AC;
-        this.Second_AC=Second_AC;
-        this.Third_AC=Third_AC;
-        this.Sleeper=Sleeper;
-        this.sDate=sDate;
-        this.i=i;
+    public DisplayTrainsResponse(ArrayList<String> train_ID, ArrayList<String> train_Name, ArrayList<String> departure, ArrayList<String> arrival, ArrayList<String> first_AC, ArrayList<String> second_AC, ArrayList<String> third_AC, ArrayList<String> sleeper, String sDate, String source, String destination,ArrayList<Integer>AC1Fare,ArrayList<Integer> AC2Fare,ArrayList<Integer>AC3Fare,ArrayList<Integer> SLFare) {
+        Train_ID = train_ID;
+        Train_Name = train_Name;
+        Departure = departure;
+        Arrival = arrival;
+        First_AC = first_AC;
+        Second_AC = second_AC;
+        Third_AC = third_AC;
+        Sleeper = sleeper;
+        this.sDate = sDate;
+        Source = source;
+        Destination = destination;
+        this.AC1Fare=AC1Fare;
+        this.AC2Fare=AC2Fare;
+        this.AC3Fare=AC3Fare;
+        this.SLFare=SLFare;
+    }
+
+    public ArrayList<Integer> getAC1Fare() {
+        return AC1Fare;
+    }
+
+    public ArrayList<Integer> getAC2Fare() {
+        return AC2Fare;
+    }
+
+    public ArrayList<Integer> getAC3Fare() {
+        return AC3Fare;
+    }
+
+    public ArrayList<Integer> getSLFare() {
+        return SLFare;
     }
 
     public ArrayList<String> getTrain_ID() {
@@ -33,24 +49,12 @@ public class DisplayTrainsResponse extends Response implements Serializable
         return Train_Name;
     }
 
-    public String getSource() {
-        return Source;
-    }
-
     public ArrayList<String> getDeparture() {
         return Departure;
     }
 
-    public String getDestination() {
-        return Destination;
-    }
-
     public ArrayList<String> getArrival() {
         return Arrival;
-    }
-
-    public String getsDate() {
-        return sDate;
     }
 
     public ArrayList<String> getFirst_AC() {
@@ -69,25 +73,16 @@ public class DisplayTrainsResponse extends Response implements Serializable
         return Sleeper;
     }
 
-    public int getI() {
-        return i;
+    public String getsDate() {
+        return sDate;
+    }
+
+    public String getSource() {
+        return Source;
+    }
+
+    public String getDestination() {
+        return Destination;
     }
 }
 
-class AvailableSeats implements Serializable
-{
-    private String Coach,Seats;
-    AvailableSeats(String Coach,String Seats)
-    {
-        this.Coach=Coach;
-        this.Seats=Seats;
-    }
-
-    public String getCoach() {
-        return Coach;
-    }
-
-    public String getSeats() {
-        return Seats;
-    }
-}
--- LockdownTravellerServer/src/DisplayTrainsResponse.java
+++ LockdownTravellerServer/src/DisplayTrainsResponse.java
@@ -6,34 +6,6 @@ public class DisplayTrainsResponse extends Response implements Serializable
 {
     final private ArrayList<String> Train_ID,Train_Name,Departure,Arrival,First_AC,Second_AC,Third_AC,Sleeper;
     final private String sDate,Source,Destination;
-<<<<<<< HEAD
-    final private int i;
-
-    public DisplayTrainsResponse(ArrayList<String> Train_ID, ArrayList<String> Train_Name, String Source,
-                                 ArrayList<String> Departure, String Destination, ArrayList<String> Arrival,
-                                 ArrayList<String> First_AC, ArrayList<String> Second_AC, ArrayList<String> Third_AC,
-                                 ArrayList<String> Sleeper, String sDate, int i) {
-        this.Train_ID=Train_ID;
-        this.Train_Name=Train_Name;
-        this.Source=Source;
-        this.Departure=Departure;
-        this.Destination=Destination;
-        this.Arrival=Arrival;
-        this.First_AC=First_AC;
-        this.Second_AC=Second_AC;
-        this.Third_AC=Third_AC;
-        this.Sleeper=Sleeper;
-        this.sDate=sDate;
-        this.i=i;
-    }
-
-    public ArrayList<String> getTrain_ID() {
-        return Train_ID;
-    }
-
-    public ArrayList<String> getTrain_Name() {
-        return Train_Name;
-=======
     final private ArrayList<Integer>AC1Fare, AC2Fare,AC3Fare,SLFare;
 
     public DisplayTrainsResponse(ArrayList<String> train_ID, ArrayList<String> train_Name, ArrayList<String> departure, ArrayList<String> arrival, ArrayList<String> first_AC, ArrayList<String> second_AC, ArrayList<String> third_AC, ArrayList<String> sleeper, String sDate, String source, String destination,ArrayList<Integer>AC1Fare,ArrayList<Integer> AC2Fare,ArrayList<Integer>AC3Fare,ArrayList<Integer> SLFare) {
@@ -64,33 +36,22 @@ public class DisplayTrainsResponse extends Response implements Serializable
 
     public ArrayList<Integer> getAC3Fare() {
         return AC3Fare;
->>>>>>> f52dd2baa11b14f5d1c24046f6a11a9828585f23
     }
 
     public ArrayList<Integer> getSLFare() {
         return SLFare;
     }
 
-<<<<<<< HEAD
-    public ArrayList<String> getDeparture() {
-        return Departure;
-=======
     public ArrayList<String> getTrain_ID() {
         return Train_ID;
->>>>>>> f52dd2baa11b14f5d1c24046f6a11a9828585f23
     }
 
     public ArrayList<String> getTrain_Name() {
         return Train_Name;
     }
 
-<<<<<<< HEAD
-    public ArrayList<String> getArrival() {
-        return Arrival;
-=======
     public ArrayList<String> getDeparture() {
         return Departure;
->>>>>>> f52dd2baa11b14f5d1c24046f6a11a9828585f23
     }
 
     public ArrayList<String> getArrival() {
