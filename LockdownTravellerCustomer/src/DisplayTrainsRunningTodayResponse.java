7efedabe175f515ea2235529c8d93ef47baf90c9
singhabhyudita <singhabhyudita@gmail.com>
2020-10-30 14:34:51 +0530

    same changes in customer

---
+++ LockdownTravellerCustomer/src/DisplayTrainsRunningTodayResponse.java
@@ -0,0 +1,41 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class DisplayTrainsRunningTodayResponse extends Response implements Serializable {
+    ArrayList<String>trainID,trainName;
+    ArrayList<ArrayList<String>>station,departure,arrival;
+    ArrayList<ArrayList<Integer>>stationNo;
+
+    public DisplayTrainsRunningTodayResponse(ArrayList<String> trainID, ArrayList<String> trainName, ArrayList<ArrayList<String>> station, ArrayList<ArrayList<Integer>> stationNo, ArrayList<ArrayList<String>> departure, ArrayList<ArrayList<String>> arrival) {
+        this.trainID = trainID;
+        this.trainName = trainName;
+        this.station = station;
+        this.stationNo = stationNo;
+        this.departure = departure;
+        this.arrival = arrival;
+    }
+
+    public ArrayList<String> getTrainID() {
+        return trainID;
+    }
+
+    public ArrayList<String> getTrainName() {
+        return trainName;
+    }
+
+    public ArrayList<ArrayList<String>> getStation() {
+        return station;
+    }
+
+    public ArrayList<ArrayList<Integer>> getStationNo() {
+        return stationNo;
+    }
+
+    public ArrayList<ArrayList<String>> getDeparture() {
+        return departure;
+    }
+
+    public ArrayList<ArrayList<String>> getArrival() {
+        return arrival;
+    }
+}
